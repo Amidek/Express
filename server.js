ae34c0b9c1596afdd78685e3ae34418a033446b4
Mariusz Jabłoński <amidek321@gmail.com>
2020-01-15 23:25:58 +0100

    rozwijanie podstrony kontaktu

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.static(path.join(__dirname + '/views/layout')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -18,6 +21,18 @@ app.get('/about', (req, res) => {
   
 app.get('/contact', (req, res) => {
     res.render('contact');
+});
+
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, image, message} = req.body;
+
+  if(author && sender && title && message && image) {
+    res.render('contact', { isSent: true });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.get('/info', (req, res) => {
@@ -31,6 +46,7 @@ app.get('/history', (req, res) => {
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name } );
 });
+
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
