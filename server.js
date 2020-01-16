1819ae824495096a9f077e32f8336f5fa72e41b5
Mariusz Jabłoński <amidek321@gmail.com>
2020-01-16 21:47:13 +0100

    poprawa wysyłania danych

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer  = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 const app = express();
 app.engine('.hbs', hbs());
@@ -8,7 +10,7 @@ app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.static(path.join(__dirname + '/views/layout')));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -18,17 +20,18 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about');
 });
-  
+
 app.get('/contact', (req, res) => {
     res.render('contact');
 });
 
-app.post('/contact/send-message', (req, res) => {
-
+app.post('/contact/send-message', upload.none(), (req, res) => {
   const { author, sender, title, image, message} = req.body;
 
+  console.log(req.body);
+
   if(author && sender && title && message && image) {
-    res.render('contact', { isSent: true });
+    res.render('contact', { isSent: true, image: req.params.image });
   }
   else {
     res.render('contact', { isError: true });
